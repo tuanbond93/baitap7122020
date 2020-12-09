4f806554775cec29c5720e3e6e5de274939be614
Tuan <sontuandav@gmail.com>
2020-12-09 19:28:46 +0700

    them tank va game1

--- tank/game1.js
+++ tank/game1.js
@@ -1,10 +1,19 @@
-function Game() {
+function Game(canvas) {
     
     this.draw = function() {
-
+        clearCanvas()
+        drawTank()
     }
 
     this.requestNextFrame = function () {
-        
+
+    }
+
+    function clearCanvas() {
+        ctx.clearRect(0, 0, parseInt(canvas.width), parseInt(canvas.height));
+    }
+
+    function drawTank() {
+        new Tank()
     }
 }
